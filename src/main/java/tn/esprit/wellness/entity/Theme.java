ec08fb8a1b674a4c6fe139e39516c81035d55b0e
Ahmed Msai <ahmed.msai2@gmail.com>
2022-07-02 00:02:12 +0200

    init project

---
+++ src/main/java/tn/esprit/wellness/entity/Theme.java
@@ -0,0 +1,5 @@
+package tn.esprit.wellness.entity;
+
+public enum Theme {
+	SPORT,MIND,ART
+}
---
+++ src/test/java/tn/esprit/wellness/WellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.wellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
