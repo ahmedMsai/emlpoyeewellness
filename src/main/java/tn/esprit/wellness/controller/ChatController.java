a5e8662a72b7092bfca020977426494e4903a206
Ahmed Msai <ahmed.msai2@gmail.com>
2022-07-22 01:11:52 +0200

    fix login

--- src/main/java/tn/esprit/wellness/controller/ChatController.java
+++ src/main/java/tn/esprit/wellness/controller/ChatController.java
@@ -4,8 +4,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import tn.esprit.wellness.pojo.ChatMessage;
+import tn.esprit.wellness.security.services.UserDetailsImpl;
 
 /**
  * Created by rajeevkumarsingh on 24/07/17.
@@ -16,6 +18,7 @@ public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+    	
         return chatMessage;
     }
 
